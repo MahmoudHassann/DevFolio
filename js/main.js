5c1d0bbca3d42b468f9c1a7ecf348b1e388a73b3
MahmoudHassann <mahmoud.m.azab@gmail.com>
2023-04-01 16:37:54 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function(){
+
+
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            
+            $('.filter').show('1000');
+        }
+        else
+        {
+          console.log($('.filter').filter('.'+value).show('3000'));
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+    }
+    
+    $(this).addClass("active");
+
+
+    
+
+});
+
+
